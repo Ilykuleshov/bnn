1d00d44316e1a6617478f93fb4e98a7ee29cce62
abazarova <a.bazarova@ira-labs.com>
2022-12-11 13:13:38 +0300

    add convolutional layer

---
+++ bnn/conv.py
@@ -0,0 +1,48 @@
+import math
+
+import torch
+import torch.nn.init as init
+from torch.nn import Module, Parameter
+import torch.nn.functional as F
+
+from torch.nn.modules.utils import _single, _pair, _triple
+
+class BayesConv2d(nn.Conv2d):
+    r"""
+    Applies Bayesian Convolution for 2D inputs
+
+    Arguments:
+        prior_mu (Float): mean of prior normal distribution.
+        prior_sigma (Float): sigma of prior normal distribution.
+
+    .. note:: other arguments are following conv of pytorch 1.2.0.
+    https://github.com/pytorch/pytorch/blob/master/torch/nn/modules/conv.py
+    
+    """
+    def __init__(self, 
+                 prior_mu, 
+                 prior_sigma, 
+                 in_channels, 
+                 out_channels, 
+                 kernel_size, 
+                 stride=1, 
+                 padding=0, 
+                 dilation=1, 
+                 padding_mode='zeros', 
+                 groups=1):
+        
+        super(BayesConv2d, self).__init__(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+            dilation=dilation,
+            padding_mode=padding_mode)
+        
+        self.prior_mu = prior_mu
+        self.prior_sigma = prior_sigma
+        m = MultivariateNormal(torch.ones(self.weight.numel()) * prior_mu,
+                              torch.eye(self.weight.numel()) * prior_sigma)
+        sampled_weights = m.rsample()
+        self.weight = Parameter(sampled_weights.reshape(self.weight.shape))
