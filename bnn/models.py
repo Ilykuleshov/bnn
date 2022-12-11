4a38502a38323fad9c947c607c943c0a6f550ee4
abazarova <a.bazarova@ira-labs.com>
2022-12-11 22:07:34 +0300

    add FCNN to models.py

--- bnn/models.py
+++ bnn/models.py
@@ -46,3 +46,45 @@ class CNN(nn.Module):
         x = x.view(-1, 1600)
         x = self.fc(x)
         return x
+    
+    
+    
+class FCNN(nn.Module):
+    def __init__(self, 
+                 weight_distribution,
+                 bias_distribution):
+        super().__init__()
+        self.fc_0 = BayesLinear(weight_distribution, bias_distribution, 
+                                  in_features = 28*28, out_features=100)
+        self.fc_1 = BayesLinear(weight_distribution, bias_distribution, 
+                                  in_features = 100, out_features=100)
+        self.fc_2 = BayesLinear(weight_distribution, bias_distribution, 
+                                  in_features = 100, out_features=10)
+        self.relu = nn.ReLU()
+        
+        self._weight_distribution = weight_distribution
+        self._bias_distribution = bias_distribution
+        
+    def weight_distribution(self):
+        return self._weight_distribution
+    
+    def bias_distribution(self):
+        return self._bias_distribution
+    
+    def weight(self):
+        return self.weight
+        
+    def log_prior(self):
+        log_p = 0
+        for m in self.modules():
+            if isinstance(m, (BayesLinear, BayesConv2d)):
+                log_p += m.log_prior()
+                
+        return log_p
+
+    def forward(self, x):
+        x = x.view(-1, 28 * 28)
+        x = self.relu(self.fc_0(x))
+        x = self.relu(self.fc_1(x))
+        x = self.fc_2(x)
+        return x
